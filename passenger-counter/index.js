0adb4702546844217078868d13412e8453053a47
CodeStation5 <js21.xda@gmail.com>
2023-07-01 00:30:05 -0700

    passenger-counter complete JS counter + basic CSS

--- passenger-counter/index.js
+++ passenger-counter/index.js
@@ -1,1 +1,18 @@
-document.getElementById("passengerCount").innerText = 5
+
+
+
+let totalCount = document.getElementById("passengerCount")
+let savedCount = document.getElementById("save-el")
+
+let count = 0;
+function increment() {
+    count += 1
+    totalCount.textContent = count  
+}
+
+function save() {
+    let countStr = count + ", "
+    savedCount.textContent += countStr
+    totalCount.textContent = 0
+    count = 0
+}
